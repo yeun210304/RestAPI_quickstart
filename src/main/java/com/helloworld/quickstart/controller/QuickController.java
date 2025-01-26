48b7c3d367be40476a4e2ae0f7b95f46d851869d
Yeun <80025908+yeun210304@users.noreply.github.com>
2025-01-26 18:29:23 +0900

    환경설정
    의존성 web, Swagger, Lombok 추가

---
+++ src/main/java/com/helloworld/quickstart/controller/QuickController.java
@@ -0,0 +1,23 @@
+package com.helloworld.quickstart.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class QuickController {
+
+    @GetMapping("/dummy")
+    public String dummy() {
+        log.info("dummy");
+        return "{}";
+    }
+
+    @GetMapping("/dummy2")
+    public String dummy2() {
+        log.info("dummy2");
+        return "dummy2";
+    }
+}
